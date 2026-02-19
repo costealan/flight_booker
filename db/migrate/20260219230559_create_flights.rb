123affd45c30a427895ff0c92664baf928f86af3
Alan <{your_public_email}@users.noreply.github.com>
2026-02-19 17:21:21 -0600

    add Airport foreing keys to Flight migration

--- db/migrate/20260219230559_create_flights.rb
+++ db/migrate/20260219230559_create_flights.rb
@@ -3,8 +3,8 @@ class CreateFlights < ActiveRecord::Migration[8.1]
     create_table :flights do |t|
       t.integer :num_passengers
       t.datetime :date
-      t.references :departing_airport, null: false, foreign_key: true
-      t.references :arriving_airport, null: false, foreign_key: true
+      t.references :departing_airport, null: false, foreign_key: { to_table: :airports }
+      t.references :arriving_airport, null: false, foreign_key: { to_table: :airports }
 
       t.timestamps
     end
