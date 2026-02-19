610d188e68d96e8f916ef036d3454bc1b9d6d448
Alan <{your_public_email}@users.noreply.github.com>
2026-02-19 17:21:01 -0600

    add associations between Flight and Airport

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,4 @@
 class Airport < ApplicationRecord
+  has_many :arriving_flights, foreign_key: :arriving_airport_id
+  has_many :departing_flights, foreign_key: :departing_airport_id
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,4 @@
 class Flight < ApplicationRecord
-  belongs_to :departing_airport
-  belongs_to :arriving_airport
+  belongs_to :departing_airport, class_name: "Airport"
+  belongs_to :arriving_airport, class_name: "Airport"
 end
