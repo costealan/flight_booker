ef7531a42ae8e5d1fd3d47d09956066b5dc741db
Alan <{your_public_email}@users.noreply.github.com>
2026-02-20 14:49:47 -0600

    add seed scripts for Airport and Flight
    All flight instances are pushed into an array for later insertion as this makes the operation go from almost 38 seconds to 1.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Airport.delete_all
+Flight.delete_all
+
+codes =[ "ATL", "DXB", "HND", "DFW", "PVG", "LHR", "IST", "CAN",  "DEN", "ORD", "DEL", "ICN", "LAX", "CDG", "PEK", "SIN", "AMS", "MAD", "SZX", "KUL" ]
+
+codes.each do |airport|
+  Airport.find_or_create_by!(code: airport)
+end
+puts "#{Airport.count} Airports have been created"
+
+airports = Airport.all
+
+
+start_time = Time.current
+
+flights = []
+
+30.times do |day |
+  airports.each do |departure|
+    airports.each do |arrival|
+      next if departure.code == arrival.code
+      flights << {
+        departing_airport_id: departure.id,
+        arriving_airport_id: arrival.id,
+        date: Time.zone.now.beginning_of_day + day.days + rand(6..22).hours + [ 0, 15, 30, 45 ].sample.minutes
+      }
+    end
+  end
+end
+
+Flight.insert_all(flights)
+end_time = Time.current
+
+puts "Flight seeds took #{end_time - start_time}"
+
+puts "#{Flight.count} Flights have been created!"
