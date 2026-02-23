0f06bf38a273795eabf67072d5fa467e881af6e1
Alan <{your_public_email}@users.noreply.github.com>
2026-02-23 16:32:35 -0600

    add scope for different dates
    It uses SQL's native DATE to only obtain y/m/d. This way the dropdown will work with unique days and not have 10 different options per day

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :departing_airport, class_name: "Airport"
   belongs_to :arriving_airport, class_name: "Airport"
+
+  scope :available_dates, -> {
+    distinct.order(:date).pluck("DATE(date)")
+  }
 end
