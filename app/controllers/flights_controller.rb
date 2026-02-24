74034ba0467e44fa6436458500ff339e6349d1d7
Alan <{your_public_email}@users.noreply.github.com>
2026-02-23 23:51:10 -0600

    add query for search results
    number of passengers is its own variable as it breaks the search query

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,18 @@
 class FlightsController  < ApplicationController
   def index
     @available_dates = Flight.available_dates
+
     @airports = Airport.all.map { |a| [ a.code, a.id ] }
+
+    if params[:arriving_airport_id] && params[:departing_airport_id] && params[:date] && params[:num_passengers]
+      selected_date = Date.parse(params[:date])
+
+      @flights = Flight.where(
+                departing_airport_id: params[:departing_airport_id],
+                arriving_airport_id: params[:arriving_airport_id],
+                date: selected_date.beginning_of_day..selected_date.end_of_day,
+      )
+      @num_passengers = params[:num_passengers]
+    end
   end
 end
