74cf6a8d1a3eaeda17a2c0e6eda83750d5e23ce7
Alan <{your_public_email}@users.noreply.github.com>
2026-02-24 16:44:49 -0600

    fix booking typo
    it had passenger instead of passengers and it broke the app when booking#new form was sent

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :flight
   has_many :passengers
-  accepts_nested_attributes_for :passenger, allow_destroy: true
+  accepts_nested_attributes_for :passengers, allow_destroy: true
 end
