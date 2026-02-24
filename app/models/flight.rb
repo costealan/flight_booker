778682d2742f3b1666c6b0cfad628b248fd2438c
Alan <{your_public_email}@users.noreply.github.com>
2026-02-24 13:06:10 -0600

    add passenger association to flight

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,7 @@ class Flight < ApplicationRecord
   belongs_to :departing_airport, class_name: "Airport"
   belongs_to :arriving_airport, class_name: "Airport"
   has_many :bookings
+  has_many :passengers, through: :bookings
 
   scope :available_dates, -> {
     distinct.order(:date).pluck("DATE(date)")
