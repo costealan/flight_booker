4e122fdcbc020dbd7692851036dc95a5cf1f4375
Alan <{your_public_email}@users.noreply.github.com>
2026-02-25 13:44:57 -0600

    add #new, #create and #show actions for bookings
    Bookings accepts Passengers attributes as the new booking form has a nested passenger form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,24 @@
 class BookingsController < ApplicationController
+  def show
+    @booking = Booking.find(params[:id])
+  end
   def new
     @booking = Booking.new
     @flight = Flight.find(params[:flight_id])
-    @num_passengers = params[:num_passengers].to_i
+    params[:num_passengers].to_i.times { @booking.passengers.build }
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  private
+  def booking_params
+    params.expect(booking: [ :flight_id, passengers_attributes: [ [ :name, :email ] ] ])
   end
 end
