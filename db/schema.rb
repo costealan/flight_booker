f65eab1fd17a1014e6d76487805f89fa6e04ec63
Alan <{your_public_email}@users.noreply.github.com>
2026-06-03 21:12:47 -0600

    add passenger schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_24_181259) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_04_023659) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -42,6 +42,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_24_181259) do
     t.string "name"
     t.datetime "updated_at", null: false
     t.index ["booking_id"], name: "index_passengers_on_booking_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "login"
+    t.string "name"
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "bookings", "flights"
